23f4370cc67c64bd7681aeb1b2b0a59445e67c56
ironcadiz <afcadiz@uc.cl>
2018-04-14 17:35:19 -0300

    added create repo route

--- src/routes/repository.js
+++ src/routes/repository.js
@@ -5,12 +5,23 @@ const router = new KoaRouter()
 
 router.use(auth)
 
-//Devolver un repositorio determinado
-//Devolver todos los repositorios de un usuario
+// mostrar todos los repos de un usuario.
+
+//mostrar cierto repo.?
+
 //Agregar un repositorio a la plataforma
+router.post("repository create", "/", async ctx => {
+    const token = ctx.headers.token
+    const user = ctx.state.currentUser
+    const { repoId, name} = ctx.request.body
+    const newRepo = await ctx.orm.Repository.create({repoId,name,ownerId: user.id,})
+    ctx.body = {repository:newRepo}        
+})
+
+
+
 //Editar un repositorio a la plataforma
 //Eliminar un repositorio
 //Crear el webhook
-
 
 module.exports = router
