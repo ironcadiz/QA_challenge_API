c80855c051ecd18ad82b57d2e9c36f32d53830c1
ironcadiz <afcadiz@uc.cl>
2018-04-14 15:08:04 -0300

    added commit model (#2)

---
+++ src/migrations/20180414180105-create-commit.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Commits', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      commitId: {
+        type: Sequelize.STRING
+      },
+      score: {
+        type: Sequelize.INTEGER
+      },
+      branch: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      repositoryId: {
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        allowNull: false,
+        references: {
+          model: 'Repositories',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Commits');
+  }
+};
