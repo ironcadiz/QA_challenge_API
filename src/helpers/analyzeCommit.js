64abbe7fe6fe52898b7acd0ea3b945ece261d6aa
ironcadiz <afcadiz@uc.cl>
2018-04-15 07:21:26 -0300

    updated scores

--- src/helpers/analyzeCommit.js
+++ src/helpers/analyzeCommit.js
@@ -7,7 +7,19 @@ const analyze = async function(commit,repo,ctx) {
   await git.clone_branch(`src/public/repos/${commit.id}`,repo.fullName,commit.branch)
   await lint(commit,ctx)
   await message(commit,ctx)
-  tasks(commit,ctx)
+  await tasks(commit,ctx)
+  const sum = await ctx.orm.Score.sum("value",{where:{commitId:commit.id}})
+  console.log(sum)
+  if (!repo.score) {
+    commit.score = sum
+  } else {
+    commit.score = sum - repo.score
+  }
+  repo.score = sum
+  await repo.save()
+  await commit.save()
+  console.log(commit.score)
+  console.log(repo.score)
 }
 
 module.exports = analyze
--- src/helpers/tasks.js
+++ src/helpers/tasks.js
@@ -1,17 +1,19 @@
 const findInFiles = require("find-in-files");
+const Promise = require("bluebird")
 
 const analyzeTask = async function(commit, ctx,taskWord,taskValue){
   const results = await findInFiles.findSync(taskWord,`src/public/repos/${commit.id}`)
-  Object.entries(results).forEach((result) => {
+  await new Promise((res) => Object.entries(results).forEach(async (result, i, arr) => {
     if (results[result[0]].count > 0) {
-      ctx.orm.Score.create({type:"task",message:`${results[result[0]].count} ${taskWord} in ${result[0]}`, value:taskValue * results[result[0]].count,commitId:commit.id})
+      await ctx.orm.Score.create({type:"task",message:`${results[result[0]].count} ${taskWord} in ${result[0]}`, value:taskValue * results[result[0]].count,commitId:commit.id})
     }
-  });
+    if(i === arr.length-1) res()
+  }));
   }
 
 const analyzeTasks = async function(commit,ctx){
   for(let i=0; i < ctx.state.rules.tasks.words.length;i++){
-    analyzeTask(commit,ctx,ctx.state.rules.tasks.words[i],ctx.state.rules.tasks.value[i])
+    await analyzeTask(commit,ctx,ctx.state.rules.tasks.words[i],ctx.state.rules.tasks.value[i])
   }
 }
 
