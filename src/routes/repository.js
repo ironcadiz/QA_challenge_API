c73bc183b7141944bbe665a0d527f86d180e6b29
rofasslerstudent <rfassler@alumnos.uai.cl>
2018-04-15 02:22:24 -0300

    Create webhook

--- src/routes/repository.js
+++ src/routes/repository.js
@@ -1,5 +1,6 @@
 const KoaRouter = require("koa-router")
 const auth = require("../middlewares/authentication")
+const request = require("superagent")
 
 const router = new KoaRouter()
 
@@ -11,14 +12,53 @@ router.use(auth)
 
 //Agregar un repositorio a la plataforma
 router.post("repository create", "/", async ctx => {
-  const token = ctx.headers.token
   const user = ctx.state.currentUser
-  const { repoId, name} = ctx.request.body
-  const newRepo = await ctx.orm.Repository.create({repoId,name,ownerId: user.id,})
-  ctx.body = { repository:newRepo }
+  const { name } = ctx.request.body
+  const repo = await request
+    .get("https://api.github.com/repos/" + user.name + "/" + name)
+    .set("Content-Type", "application/json")
+    .set("Authorization", "Bearer " + user.gitToken)
+
+  const repository = await ctx.orm.Repository.find({ where: { name } })
+  if (!repository) {
+    ctx.body = {
+      error: true,
+      message: "El repositorio ya se encuentra agregado",
+    }
+  } else {
+    ctx.body = { repository: repository }
+    var repoId = repo.body.id
+    var score = 0
+    var url = repo.body.url
+    var fullName = repo.body.full_name
+
+    const newRepo = await ctx.orm.Repository.create({
+      name,
+      fullName,
+      score,
+      url,
+      repoId,
+      ownerId: user.id,
+    })
+    const hook = await request
+      .post("https://api.github.com/repos/" + user.name + "/" + name + "/hooks")
+      .set("Content-Type", "application/json")
+      .set("Authorization", "Bearer " + user.gitToken)
+      .send({
+        name: "web",
+        active: true,
+        events: ["push", "pull_request"],
+        config: {
+          url: "http://localhost:3000/commits",
+          content_type: "json",
+        },
+      })
+    ctx.body = {
+      error: false,
+      repository: newRepo,
+    }
+  }
 })
-
-
 
 //Editar un repositorio a la plataforma
 //Eliminar un repositorio
