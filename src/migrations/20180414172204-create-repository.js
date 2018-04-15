3b118744e1c6719ea69f89b8ceb67c1ab58d1af6
ironcadiz <afcadiz@uc.cl>
2018-04-14 21:50:15 -0300

    Added repo cloning on commit create

--- src/migrations/20180414172204-create-repository.js
+++ src/migrations/20180414172204-create-repository.js
@@ -11,6 +11,9 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
+      fullName:{
+        type: Sequelize.STRING
+      },
       score: {
         type: Sequelize.INTEGER
       },
