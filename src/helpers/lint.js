ed318e0381a05728df2a77e70de15624019a3074
ironcadiz <afcadiz@uc.cl>
2018-04-15 00:57:03 -0300

    linter runs and ouput is parsed to json.

--- src/helpers/lint.js
+++ src/helpers/lint.js
@@ -1,7 +1,15 @@
 const exec = require("child_process")
+const fs = require("fs")
+
+const parseFile = function(file){
+  const data = fs.readFileSync(file, 'utf8')
+  const json = data.slice(data.indexOf(`[{`))
+  return JSON.parse(json)
+}
 
 const runLint = async function(commit) {
-  exec.execSync(`npm run --prefix src/public/repos/${commit.id}/  lint -- -f json >> src/public/lint/${commit.id}.json` )
+  exec.execSync(`npm run --prefix src/public/repos/${commit.id}/  lint -- -f json > src/public/lint/${commit.id}.json` )
+  const json = parseFile(`src/public/lint/${commit.id}.json`)
 }
 
 module.exports = runLint
