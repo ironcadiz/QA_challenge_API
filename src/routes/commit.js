4e0f77eb70858b25bb4d266124c24cfb2e7af7c1
Gabriel Ulloa <gsulloa@uc.cl>
2018-04-15 02:19:38 -0300

    Read paths (#9)
    * Obtain all my commits by repository
    
    * Order to not crash

--- src/routes/commit.js
+++ src/routes/commit.js
@@ -1,11 +1,35 @@
 const KoaRouter = require("koa-router")
 const auth = require("../middlewares/authentication")
 const parseRules = require("../middlewares/rules")
+const _ = require("lodash")
 const analize = require("../helpers/analyzeCommit")
 
 const router = new KoaRouter()
 
-const create_commits = async (ctx,commits,repo,ref) => {
+router.use("/all", auth)
+
+router.get("commits", "/all", async ctx => {
+  console.log("user id", ctx.state.currentUser)
+  const commits = await ctx.orm.Commit.findAll({
+    where: {
+      userId: ctx.state.currentUser.id,
+    },
+  })
+
+  ctx.body = _.reduce(
+    commits,
+    (prev, next) => {
+      if (!prev[next.repositoryId]) prev[next.repositoryId] = []
+      return {
+        ...prev,
+        [next.repositoryId]: [...prev[next.repositoryId], next],
+      }
+    },
+    {}
+  )
+})
+
+const create_commits = async (ctx, commits, repo, ref) => {
   const promises = commits.map(async commit => {
     const commiter = await ctx.orm.user.findOne({ where: {email: commit.author.email }})
     const dbCommit = await ctx.orm.Commit.create({userId:commiter.id, repositoryId: repo.id, commitId:commit.id, branch:ref.split("/").slice(-1)[0]})
@@ -20,7 +44,9 @@ router.use(parseRules)
 router.post("commit_create", "/", async ctx => {
   const { ref, commits, repository } = ctx.request.body
   const response = {}
-  const repo = await ctx.orm.Repository.findOne({ where: { repoId: repository.id } })
+  const repo = await ctx.orm.Repository.findOne({
+    where: { repoId: repository.id },
+  })
   if (repo) {
     create_commits(ctx, commits, repo,ref)
   } else {
@@ -31,7 +57,5 @@ router.post("commit_create", "/", async ctx => {
   }
   ctx.body = response
 })
-
-router.use(auth)
 
 module.exports = router
